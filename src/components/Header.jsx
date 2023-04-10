c4b97aeee45c8be226a58de203c0337c15032861
tarikul-islam <tariikul12@gmail.com>
2023-04-10 10:47:50 +0600

    Responsive navbar Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import {
+  BoltIcon,
+  Bars3BottomRightIcon,
+  XMarkIcon,
+  LinkIcon
+  
+} from '@heroicons/react/24/solid'
 
 const Header = () => {
-    return (
-        <div>
-            <h4>hello</h4>
-        </div>
-    );
-};
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  return (
+    <div className='bg-gray-100 px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+      <div className='relative flex items-center justify-between'>
+        {/* Logo Section */}
+        <Link to='/' className='inline-flex items-center'>
+          <LinkIcon className='h-6 w-6 text-blue-500' />
+          <span className='ml-2 text-xl font-bold tracking-wide text-gray-800'>
+          Glass-door
+          </span>
+        </Link>
 
-export default Header;
+        {/* Nav Items Section */}
+        <ul className='items-center hidden space-x-8 lg:flex'>
+          <li>
+            <NavLink
+              to='/'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+             Statistics
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/books'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Applied Jobs
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/about'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Blog
+            </NavLink>
+          </li>
+        </ul>
+        <button className="btn btn-primary">Star Applying</button>
+        {/* Mobile Navbar Section */}
+        <div className='lg:hidden'>
+          {/* Dropdown Open Button */}
+          <button
+            aria-label='Open Menu'
+            title='Open Menu'
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <Bars3BottomRightIcon className='w-5 text-gray-600' />
+          </button>
+          {isMenuOpen && (
+            <div className='absolute top-0 left-0 w-full z-10'>
+              <div className='p-5 bg-white border rounded shadow-sm'>
+                {/* Logo & Button section */}
+                <div className='flex items-center justify-between mb-4'>
+                  <div>
+                    <Link to='/' className='inline-flex items-center'>
+                      <BoltIcon className='h-6 w-6 text-blue-500' />
+                      <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
+                        Glass-door
+                      </span>
+                    </Link>
+                  </div>
+                  {/* Dropdown menu close button */}
+                  <div>
+                    <button
+                      aria-label='Close Menu'
+                      title='Close Menu'
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <XMarkIcon className='w-5 text-gray-600' />
+                    </button>
+                  </div>
+                </div>
+                {/* Mobile Nav Items Section */}
+                <nav>
+                  <ul className='space-y-4'>
+                    <li>
+                      <Link to='/' className='default'>
+                      Statistics
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/books'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        Applied Jobs
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/about'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        Blog
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
